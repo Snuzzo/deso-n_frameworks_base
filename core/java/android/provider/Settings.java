18b21d95638be522b3fce8057617482ea757f831
PMS22 <prathams99@rediff.com>
2017-02-07 16:43:13 +0000

    Hide Lockscreen emergency button [1/2]
    *DU edits/notes*
    
    - Hiding the emergency button is something that some users wish to have.
    I don't get it but it can be done while keeping the stock behavior by default
    
    - Removed unnecessary imports
    - Removed unnecessary void used in the original patch
    - Kept the original argument for 'IsInCall' which was removed in original patch
    
    Honestly this was a train wreck when we got it but it works good now :-)
    
    Change-Id: I908e5b16faf2912842d7a8a3c657ecca9b76baaf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4542,6 +4542,12 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Show/Hide Emergency Button on Lockscreen
+         * @hide
+         */
+        public static final String SHOW_EMERGENCY_BUTTON = "show_emergency_button";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
